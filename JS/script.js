d4fb5f4f5d60a05e960a2bccd808ec598820eae1
Marek Sendek <marek.sendek@gmail.com>
2022-11-16 06:03:55 +0100

    last modifications

--- JS/script.js
+++ JS/script.js
@@ -6,6 +6,6 @@ let themeName = document.querySelector(".themeName");
 
 button.addEventListener("click", () => {
     container.classList.toggle("orange");
-    if (container.classList.contains("orange")) { themeName.innerText = "niebieski"; }
-    else { themeName.innerText = "pomaranczowy" }
+    if (container.classList.contains("orange")) { themeName.innerText = "jasne"; }
+    else { themeName.innerText = "pomaranczowe" }
 }); 
